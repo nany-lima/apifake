54c3a7a6102fed17f82aa55bd7dd35368719305b
Thauanny Lima <mtsl2@aluno.ifal.edu.br>
2026-09-16 16:47:29 -0300

    Add files via upload

---
+++ apifake.json.dart
@@ -0,0 +1,64 @@
+{
+"ranking": [
+{
+"id": "1",
+"name": "Nany",
+"level": "LV.13"
+},
+{
+"id": "2",
+"name": "Carol",
+"level": "LV.13"
+},
+{
+"id": "3",
+"name": "Levi",
+"level": "LV.13"
+},
+{
+"id": "4",
+"name": "Manoel",
+"level": "LV.13"
+},
+{
+"id": "5",
+"name": "Brenda",
+"level": "LV.13"
+},
+{
+"id": "6",
+"name": "Ysa",
+"level": "LV.13"
+},
+{
+"id": "7",
+"name": "Davi",
+"level": "LV.13"
+},
+{
+"id": "8",
+"name": "havi",
+"level": "LV.13"
+},
+{
+"id": "9",
+"name": "Luna",
+"level": "LV.13"
+},
+{
+"id": "10",
+"name": "Kety",
+"level": "LV.13"
+},
+{
+"id": "11",
+"name": "Lany",
+"level": "LV.13"
+},
+{
+"id": "12",
+"name": "Maya",
+"level": "LV.13"
+}
+]
+}
